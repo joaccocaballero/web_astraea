2839a89739ed167b23fa29f3a00fdd019c2d0f53
joaccocaballero <joaquincaballero182@gmail..com>
2024-08-11 13:30:34 -0300

    fix components

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 "use client";
 import React from 'react';
-import Nosotros from '../../components/nosotros';
-import Inicio from '../../components/inicio';
-import NavBar from '../../components/navbar';
+import Nosotros from '@/components/nosotros';
+import Inicio from '@/components/inicio';
+import NavBar from '@/components/navbar';
 
 export default function Home() {
   return (
